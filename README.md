6ea1620fcdf2ff87e900fe91d5a76d4bb76d1f37
Marcus Chiu <marcuschiu9@gmail.com>
2017-12-23 16:42:10 -0600

    initial

---
+++ README.md
@@ -0,0 +1,3 @@
+1. make sure config server is up and running so discovery server is able to retrieve its application configurations
+2. run eureka discovery server <code>mvn spring-boot:run </code>
+3. go to localhost:8082 to view Eureka Manager
---
+++ src/main/java/com/marcuschiu/discovery/DiscoveryApplication.java
@@ -0,0 +1,19 @@
+package com.marcuschiu.discovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @EnableEurekaServer will configure this server as a discovery server using Netflix Eureka.
+ * Spring Boot will automatically detect the configuration dependency on the classpath and lookup
+ * the configuration from the config server.
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscoveryApplication.class, args);
+	}
+}
